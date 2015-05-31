fab6a8f94882275fcdfed3d4cdc377b2213bce5e
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2015-05-31 19:55:01 +0200

    add toggle method

--- dist/jquery.lpfactory-slide-panel.js
+++ dist/jquery.lpfactory-slide-panel.js
@@ -6,35 +6,28 @@
  *  Made by Jonathan Bouzekri
  *  Under MIT License
  */
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
+/*
+ *  jquery-lp-factory-slide-panel - v0.0.1
+ *  https://github.com/LpFactory
+ *
+ *  Made by Jonathan Bouzekri
+ *  Under MIT License
+ */
 ; // jshint ignore:line
 (function($, window, document, undefined) {
 
 	"use strict";
 
-	// undefined is used here as the undefined global variable in ECMAScript 3 is
-	// mutable (ie. it can be changed by someone else). undefined isn't really being
-	// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-	// can no longer be modified.
-
-	// window and document are passed through as local variable rather than global
-	// as this (slightly) quickens the resolution process and can be more efficiently
-	// minified (especially when both are regularly referenced in your plugin).
-
-	// Create the defaults once
+	// Create the defaults
 	var pluginName = "lpSlidePanel",
 		defaults = {
-			propertyName: "value"
+			expanded: false
 		};
 
 	// The actual plugin constructor
 	function Plugin(element, options) {
 		this.element = element;
-		// jQuery has an extend method which merges the contents of two or
-		// more objects, storing the result in the first object. The first object
-		// is generally empty as we don't want to alter the default options for
-		// future instances of the plugin
+		this.$element = $(element);
 		this.settings = $.extend({}, defaults, options);
 		this._defaults = defaults;
 		this._name = pluginName;
@@ -43,17 +36,25 @@
 
 	// Avoid Plugin.prototype conflicts
 	$.extend(Plugin.prototype, {
-		init: function() {
-			// Place initialization logic here
-			// You already have access to the DOM element and
-			// the options via the instance, e.g. this.element
-			// and this.settings
-			// you can add more functions like the one below and
-			// call them like so: this.yourOtherFunction(this.element, this.settings).
-			console.log("xD");
+		init: function() {},
+		show: function() {
+			this.$element.animate({
+				"margin-left": 0
+			}, "slow");
+			this.expanded = true;
 		},
-		yourOtherFunction: function() {
-			// some logic
+		hide: function() {
+			this.$element.animate({
+				"margin-left": -(this.$element.outerWidth())
+			}, "slow");
+			this.expanded = false;
+		},
+		toggle: function() {
+			if (this.expanded) {
+				this.hide();
+			} else {
+				this.show();
+			}
 		}
 	});
 
@@ -63,6 +64,10 @@
 		return this.each(function() {
 			if (!$.data(this, "plugin_" + pluginName)) {
 				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+			}
+
+			if ($.isFunction(Plugin.prototype[options])) {
+				$.data(this, "plugin_" + pluginName)[options]();
 			}
 		});
 	};
